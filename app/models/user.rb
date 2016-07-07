79134a6d0d73f0affa8f4f88dbca550820b266a3
Adrian Kuciel <djmicros@gmail.com>
2016-07-07 15:35:39 +0200

    Rollback to previous fb auth version

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,6 @@ class User < ActiveRecord::Base
       user.country = auth.info.location
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-	  user.id = User.last.id + 1
 
       if user.password_digest == nil 
       user.password_digest = "facebook"
